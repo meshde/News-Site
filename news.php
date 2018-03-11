c67abfdcc757f0648e55e4433eaf89014b62e751
meshde <meshde.md@gmail.com>
2018-03-11 10:56:18 +0530

    Complete get_feed function

--- news.php
+++ news.php
@@ -3,12 +3,35 @@ function getfeed($url){
 	$content = file_get_contents($url);
 	$xml = new SimpleXmlElement($content);
 
-	echo "<ul>";
+	$feed = array();
 	foreach($xml->channel->item as $entry){
-		echo "<li><a href='$entry->link'>".$entry->title."</a></li>";
+		$item = array (
+			'title' => $entry->title,
+			'desc' => $entry->description,
+			'link' => $entry->link,
+			'date' => $entry->pubDate,
+		);
+	array_push($feed, $item);
 	}
-	echo "</ul";
+
+	$return = '';
+	foreach($feed as $item){
+		$title = str_replace(' & ', ' &amp; ', $item['title']);
+		$link = $item['link'];
+		$description = $item['desc'];
+		$date = date('l F d, Y', strtotime($item['date']));
+
+		$return .= '<div class=\'item\'>';
+		$return .= '<p><strong><a href="'.$link.'" title="'.$title.'">'.$title.'</a></strong><br />';
+		$return .= '<small><em>Posted on '.$date.'</em></small></p>';
+		$return .= '<p>'.$description.'</p>';
+		$return .= '</div>';
+		// $return .= '<br>';
+	}
+	return $return;
 }
 ?>
 
-<?php getfeed("https://timesofindia.indiatimes.com/rssfeedstopstories.cms"); ?>
+<?php 
+// echo '<div id=1>'.getfeed("https://timesofindia.indiatimes.com/rssfeedstopstories.cms").'</div>'; 
+?>
